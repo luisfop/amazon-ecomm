74202cc3db83035729a706f0ad7b6cb8bcd724c5
luisfop <luisfop.dev@gmail.com>
2022-12-01 13:46:59 -0300

    feat: virtual component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css'
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
 import Slider from './components/slider/Slider'
+import Virtual from './components/virtual/Virtual'
+import Products from './components/products/Products'
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
       <Header />
       <Hero />
       <Slider/>
+      <Virtual/>
+      <Products/>
     </div>
   )
 }

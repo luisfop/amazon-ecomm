dbb0688a8ffe05de7347336f6a4fed08bf16d28d
luisfop <luisfop.dev@gmail.com>
2022-12-01 15:16:00 -0300

    feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 
 import reactLogo from './assets/react.svg'
 import './App.css'
+
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
 import Slider from './components/slider/Slider'
 import Virtual from './components/virtual/Virtual'
 import Products from './components/products/Products'
 import Testimonials from './components/testimonials/Testimonials'
+import Footer from './components/footer/Footer'
 
 function App() {
-
 
   return (
     <div className="App">
@@ -19,6 +20,7 @@ function App() {
       <Virtual/>
       <Products/>
       <Testimonials/>
+      <Footer/>
     </div>
   )
 }
